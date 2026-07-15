290cdde0f340003cabb4ec1f45e1b4b94901b72c
Vishal Rohilla <vishalrohilla@Vishals-MacBook-Air.local>
2026-07-15 22:04:27 +0530

    Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,16 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://sharx.in",
+      url: "https://sharx.in/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
+    },
+    {
+      url: "https://sharx.in/home",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
     },
   ];
 }
