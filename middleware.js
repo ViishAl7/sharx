0011d533ad33c774025c3afc19ef745678231265
Vishal Rohilla <vishalrohilla@Vishals-MacBook-Air.local>
2026-09-23 15:54:16 +0530

    Enable production maintenance mode

--- middleware.js
+++ middleware.js
@@ -1,6 +1,18 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
+  const hostname = request.nextUrl.hostname;
+
+  // LOCAL → normal SHARX website
+  if (
+    hostname === "localhost" ||
+    hostname === "127.0.0.1" ||
+    hostname.startsWith("192.168.")
+  ) {
+    return NextResponse.next();
+  }
+
+  // LIVE → maintenance page
   return NextResponse.rewrite(
     new URL("/maintenance", request.url)
   );
