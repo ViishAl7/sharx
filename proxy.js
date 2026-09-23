dab698d86bc8f85794ba52126af8fa17e8064ecc
Vishal Rohilla <vishalrohilla@Vishals-MacBook-Air.local>
2026-09-23 12:24:02 +0530

    Enable SHARX maintenance mode

---
+++ proxy.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export function proxy(request) {
+  return NextResponse.rewrite(
+    new URL("/maintenance", request.url)
+  );
+}
+
+export const config = {
+  matcher: [
+    "/((?!maintenance|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
